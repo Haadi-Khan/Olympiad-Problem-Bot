c8e74413f8dfcb7a8eb88eaf4655fef4a6f7e510
Haadi Khan <haadikhan05@gmail.com>
2021-07-18 19:41:23 -0400

    Resolved issue from testing

--- main.py
+++ main.py
@@ -21,24 +21,24 @@ async def on_ready():
     print('We have logged in as {0.user}'.format(client))
 
 
-# @client.command(name='pr')
-# async def rand_prob(ctx, level):
-#     try:
-#         if level == '10' or level == '12':
-#             year, version, question = randint(2002, 2021), choice(('A', 'B')), randint(1, 25)
-#             path = 'AMC/%d/%d/%s/%d.png' % (int(level), year, version, question)
-#             file = discord.File(path, filename='image.png')
-#             embed = discord.Embed(
-#                 title='AMC ' + level + version + ' Problem',
-#                 description='[Solution](https://artofproblemsolving.com/wiki/index.php/%d_AMC_%s%s_Problems/Problem_%d)' % (year, level, version, question),
-#                 color=discord.Colour.blue()
-#             )
-#             embed.set_image(url="attachment://image.png")
-#             await ctx.send(file=file, embed=embed)
-#         else:
-#             await ctx.send("Invalid Version")
-#     except ValueError:
-#         await ctx.send("Invalid Version")
+@client.command(name='pr')
+async def rand_prob(ctx, level):
+    try:
+        if level == '10' or level == '12':
+            year, version, question = randint(2002, 2021), choice(('A', 'B')), randint(1, 25)
+            path = 'AMC/%d/%d/%s/%d.png' % (int(level), year, version, question)
+            file = discord.File(path, filename='image.png')
+            embed = discord.Embed(
+                title='AMC ' + level + version + ' Problem',
+                description='[Solution](https://artofproblemsolving.com/wiki/index.php/%d_AMC_%s%s_Problems/Problem_%d)' % (year, level, version, question),
+                color=discord.Colour.blue()
+            )
+            embed.set_image(url="attachment://image.png")
+            await ctx.send(file=file, embed=embed)
+        else:
+            await ctx.send("Invalid Version")
+    except ValueError:
+        await ctx.send("Invalid Version")
 
 
 # Easy Questions
@@ -65,28 +65,28 @@ async def cat_prob_easy(ctx, cat):
         await cat_problem(ctx, level, year, version, question)
 
 
-# # Hard Questions
-# @client.command(name='pch')
-# async def cat_prob_hard(ctx, cat):
-#     if cat == 'alg':
-#         i = randint(0, len(h_alg) - 1)
-#         year, level, version, question = h_alg[i][0:4], h_alg[i][4:6], h_alg[i][6:7], h_alg[i][7:]
-#         await cat_problem(ctx, level, year, version, question)
-#
-#     if cat == 'com':
-#         i = randint(0, len(h_com) - 1)
-#         year, level, version, question = h_com[i][0:4], h_com[i][4:6], h_com[i][6:7], h_com[i][7:]
-#         await cat_problem(ctx, level, year, version, question)
-#
-#     if cat == 'geo':
-#         i = randint(0, len(h_geo) - 1)
-#         year, level, version, question = h_geo[i][0:4], h_geo[i][4:6], h_geo[i][6:7], h_geo[i][7:]
-#         await cat_problem(ctx, level, year, version, question)
-#
-#     if cat == 'nt':
-#         i = randint(0, len(h_nt) - 1)
-#         year, level, version, question = h_nt[i][0:4], h_nt[i][4:6], h_nt[i][6:7], h_nt[i][7:]
-#         await cat_problem(ctx, level, year, version, question)
+# Hard Questions
+@client.command(name='pch')
+async def cat_prob_hard(ctx, cat):
+    if cat == 'alg':
+        i = randint(0, len(h_alg) - 1)
+        year, level, version, question = h_alg[i][0:4], h_alg[i][4:6], h_alg[i][6:7], h_alg[i][7:]
+        await cat_problem(ctx, level, year, version, question)
+
+    if cat == 'com':
+        i = randint(0, len(h_com) - 1)
+        year, level, version, question = h_com[i][0:4], h_com[i][4:6], h_com[i][6:7], h_com[i][7:]
+        await cat_problem(ctx, level, year, version, question)
+
+    if cat == 'geo':
+        i = randint(0, len(h_geo) - 1)
+        year, level, version, question = h_geo[i][0:4], h_geo[i][4:6], h_geo[i][6:7], h_geo[i][7:]
+        await cat_problem(ctx, level, year, version, question)
+
+    if cat == 'nt':
+        i = randint(0, len(h_nt) - 1)
+        year, level, version, question = h_nt[i][0:4], h_nt[i][4:6], h_nt[i][6:7], h_nt[i][7:]
+        await cat_problem(ctx, level, year, version, question)
 
 
 async def cat_problem(ctx, level, year, version, question):
