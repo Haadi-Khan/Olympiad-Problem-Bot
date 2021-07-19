6f1166a52af2e8001bf00a68f655591635ec1d89
Haadi Khan <haadikhan05@gmail.com>
2021-07-19 16:24:33 -0400

    Added AIME Problems
    Well I was gonnna do this later, but I was bored and hungry
    because I was fasting lmao. Edited stuff to accomodate that after
    adding the AIME folder from webscraping. Read the comments in
    main.py if you would like to know more about how the naming
    schemes work in the txts.

--- main.py
+++ main.py
@@ -15,30 +15,54 @@ h_com = [line.strip() for line in open('categories/Hard/Combinatorics.txt')]
 h_geo = [line.strip() for line in open('categories/Hard/Geometry.txt')]
 h_nt = [line.strip() for line in open('categories/Hard/NumberTheory.txt')]
 
+'''
+ file naming scheme is Test/Year/Level/Version/Question
+ for AIMEs, threw in a 10 for the version just so I didn't have to change code, it's easier this way
+ also, the I and II of AIME naming was turned into A/B for same reason as before
+ examples: AM201810A13 (2018 AMC 10A Problem 13) | AI200210A12 (2002 AIME I Problem 12)
+ 
+ Also quick sidenote as to why there are more AIME years than AMCs:
+ AIME has had the convention of I and II for longer and this format makes it easier to webscrape due to the link style
+ being standard. Other AMCs or former AIMEs might be added in the future but for now this is the format
+'''
+
 
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
 
 
-@client.command(name='pr')
+@client.command(name='amc')
 async def rand_prob(ctx, level):
-    try:
-        if level == '10' or level == '12':
-            year, version, question = randint(2002, 2021), choice(('A', 'B')), randint(1, 25)
-            path = 'AMC/%d/%d/%s/%d.png' % (int(level), year, version, question)
-            file = discord.File(path, filename='image.png')
-            embed = discord.Embed(
-                title='AMC ' + level + version + ' Problem',
-                description='[Solution](https://artofproblemsolving.com/wiki/index.php/%d_AMC_%s%s_Problems/Problem_%d)' % (year, level, version, question),
-                color=discord.Colour.blue()
-            )
-            embed.set_image(url="attachment://image.png")
-            await ctx.send(file=file, embed=embed)
-        else:
-            await ctx.send("Invalid Version")
-    except ValueError:
-        await ctx.send("Invalid Version")
+    if level == 10 or level == 12:
+        year, version, question = randint(2002, 2021), choice(('A', 'B')), randint(1, 25)
+        path = 'AMC/%s/%d/%s/%d.png' % (level, year, version, question)
+        file = discord.File(path, filename='image.png')
+        embed = discord.Embed(
+            title='AMC ' + level + version + ' Problem',
+            description='[Solution](https://artofproblemsolving.com/wiki/index.php/%d_AMC_%s%s_Problems/Problem_%d)'
+                        % (year, level, version, question),
+            color=discord.Colour.blue()
+        )
+        embed.set_image(url="attachment://image.png")
+        await ctx.send(file=file, embed=embed)
+    else:
+        await ctx.send("Invalid Level")
+
+
+@client.command(name='aime')
+async def rand_prob(ctx):
+    year, version, question = randint(2002, 2021), choice(('I', 'II')), randint(1, 15)
+    path = 'AIME/%d/%s/%d.png' % (year, version, question)
+    file = discord.File(path, filename='image.png')
+    embed = discord.Embed(
+        title='AIME ' + version + ' Problem',
+        description='[Solution](https://artofproblemsolving.com/wiki/index.php/%d_AIME_%s_Problems/Problem_%s)'
+                    % (year, version, question),
+        color=discord.Colour.blue()
+    )
+    embed.set_image(url="attachment://image.png")
+    await ctx.send(file=file, embed=embed)
 
 
 # Easy Questions
@@ -46,23 +70,23 @@ async def rand_prob(ctx, level):
 async def cat_prob_easy(ctx, cat):
     if cat == 'alg':
         i = randint(0, len(e_alg) - 1)
-        year, level, version, question = e_alg[i][0:4], e_alg[i][4:6], e_alg[i][6:7], e_alg[i][7:]
-        await cat_problem(ctx, level, year, version, question)
+        exam, year, level, version, question = e_alg[i][0:2], e_alg[i][2:6], e_alg[i][6:8], e_alg[i][8:9], e_alg[i][9:]
+        await cat_problem(ctx, exam, level, year, version, question)
 
     if cat == 'com':
         i = randint(0, len(e_com) - 1)
-        year, level, version, question = e_com[i][0:4], e_com[i][4:6], e_com[i][6:7], e_com[i][7:]
-        await cat_problem(ctx, level, year, version, question)
+        exam, year, level, version, question = e_com[i][0:2], e_com[i][2:6], e_com[i][6:8], e_com[i][8:9], e_com[i][9:]
+        await cat_problem(ctx, exam, level, year, version, question)
 
     if cat == 'geo':
         i = randint(0, len(e_geo) - 1)
-        year, level, version, question = e_geo[i][0:4], e_geo[i][4:6], e_geo[i][6:7], e_geo[i][7:]
-        await cat_problem(ctx, level, year, version, question)
+        exam, year, level, version, question = e_geo[i][0:2], e_geo[i][2:6], e_geo[i][6:8], e_geo[i][8:9], e_geo[i][9:]
+        await cat_problem(ctx, exam, level, year, version, question)
 
     if cat == 'nt':
         i = randint(0, len(e_nt) - 1)
-        year, level, version, question = e_nt[i][0:4], e_nt[i][4:6], e_nt[i][6:7], e_nt[i][7:]
-        await cat_problem(ctx, level, year, version, question)
+        exam, year, level, version, question = e_nt[i][0:2], e_nt[i][2:6], e_nt[i][6:8], e_nt[i][8:9], e_nt[i][9:]
+        await cat_problem(ctx, exam, level, year, version, question)
 
 
 # Hard Questions
@@ -70,37 +94,51 @@ async def cat_prob_easy(ctx, cat):
 async def cat_prob_hard(ctx, cat):
     if cat == 'alg':
         i = randint(0, len(h_alg) - 1)
-        year, level, version, question = h_alg[i][0:4], h_alg[i][4:6], h_alg[i][6:7], h_alg[i][7:]
-        await cat_problem(ctx, level, year, version, question)
+        exam, year, level, version, question = h_alg[i][0:2], h_alg[i][2:6], h_alg[i][6:8], h_alg[i][8:9], h_alg[i][9:]
+        await cat_problem(ctx, exam, level, year, version, question)
 
     if cat == 'com':
         i = randint(0, len(h_com) - 1)
-        year, level, version, question = h_com[i][0:4], h_com[i][4:6], h_com[i][6:7], h_com[i][7:]
-        await cat_problem(ctx, level, year, version, question)
+        exam, year, level, version, question = h_com[i][0:2], h_com[i][2:6], h_com[i][6:8], h_com[i][8:9], h_com[i][9:]
+        await cat_problem(ctx, exam, level, year, version, question)
 
     if cat == 'geo':
         i = randint(0, len(h_geo) - 1)
-        year, level, version, question = h_geo[i][0:4], h_geo[i][4:6], h_geo[i][6:7], h_geo[i][7:]
-        await cat_problem(ctx, level, year, version, question)
+        exam, year, level, version, question = h_geo[i][0:2], h_geo[i][2:6], h_geo[i][6:8], h_geo[i][8:9], h_geo[i][9:]
+        await cat_problem(ctx, exam, level, year, version, question)
 
     if cat == 'nt':
         i = randint(0, len(h_nt) - 1)
-        year, level, version, question = h_nt[i][0:4], h_nt[i][4:6], h_nt[i][6:7], h_nt[i][7:]
-        await cat_problem(ctx, level, year, version, question)
+        exam, year, level, version, question = h_nt[i][0:2], h_nt[i][2:6], h_nt[i][6:8], h_nt[i][8:9], h_nt[i][9:]
+        await cat_problem(ctx, exam, level, year, version, question)
 
 
-async def cat_problem(ctx, level, year, version, question):
-    path = 'AMC/%s/%s/%s/%s.png' % (level, year, version, question)
-    file = discord.File(path, filename='image.png')
-    link = 'https://artofproblemsolving.com/wiki/index.php/%s_AMC_%s%s_Problems/Problem_%s' % (year, level, version, question)
-    embed = discord.Embed(
-        title='AMC ' + level + version + ' Problem',
-        description='[Solution](' + link + ')',
-        color=discord.Colour.blue()
-    )
-    embed.set_image(url="attachment://image.png")
-
-    await ctx.send(file=file, embed=embed)
+async def cat_problem(ctx, exam, level, year, version, question):
+    if exam == 'AM':
+        path = 'AMC/%s/%s/%s/%s.png' % (level, year, version, question)
+        file = discord.File(path, filename='image.png')
+        link = 'https://artofproblemsolving.com/wiki/index.php/%s_AMC_%s%s_Problems/Problem_%s' % (
+            year, level, version, question)
+        embed = discord.Embed(
+            title='AMC ' + level + version + ' Problem',
+            description='[Solution](' + link + ')',
+            color=discord.Colour.blue()
+        )
+        embed.set_image(url="attachment://image.png")
+        await ctx.send(file=file, embed=embed)
+    if exam == 'AI':
+        version = 'I' if version == 'A' else 'II'
+        path = 'AIME/%s/%s/%s.png' % (year, version, question)
+        file = discord.File(path, filename='image.png')
+        link = 'https://artofproblemsolving.com/wiki/index.php/%s_AIME_%s_Problems/Problem_%s' % (
+            year, version, question)
+        embed = discord.Embed(
+            title='AIME ' + version + ' Problem',
+            description='[Solution](' + link + ')',
+            color=discord.Colour.blue()
+        )
+        embed.set_image(url="attachment://image.png")
+        await ctx.send(file=file, embed=embed)
 
 
 client.run(TOKEN)
